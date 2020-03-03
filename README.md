bbded5cd9feb9902aa6c37d133dd930e6ebf9e01
Philippe Correia <revwhyte@gmail.com>
2020-03-03 13:58:38 -0300

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# poe-ultimate-bravery
-So I'm writing a bot to pick an insane Path of Exile build. It's fun to troll your beginner friends, so you'll have time to teach them the game properly while they're playing
+# Path of Exile: Ultimate Bravery
+I'm writing a bot to pick an insane Path of Exile build. It's fun to troll your beginner friends, so you'll have time to teach them the game properly while they're playing.
+
+This idea was based on the [Ultimate Bravery](https://ultimate-bravery.net/) for League of Legends game, made by some player from the Community.
+
+The idea is pretty simple: you run the code and it gives you character, ascension and ability.
+
+Then you must build your char around it. No crying. Just take it.
+
+## Version 1.0
+* You can roll char, ascension and main skill.
+
+## TO-DO
+* Create groups of skills (main skills, movement skills, defense skills, pet skills) and roll one of each.
+* After that, create a list of supports (must check compatibility with the skills) for each skill picked that way.
+
+## Running
+
+First, you may have Python 3+ installed in your machine, in order to run this.
+
+Then, open your terminal in this project folder, and type:
+```shell
+python pickABuild.py
+```
+
+The magic will happen at your terminal.
+
+Accept your fate. Hear the call. Save Oriath, Exile.
--- pickABuild.py
+++ pickABuild.py
@@ -2,46 +2,41 @@
 
 from random import shuffle, choice
 from time import sleep
+import gems
 
-bonecos = ['duelist','marauder','ranger','scion','shadow','templar','witch']
-ascensoes = {
-    'duelist': ['slayer', 'champion', 'gladiator'],
-    'marauder': ['chieftain', 'berserker', 'juggernaut'],
-    'ranger': ['raider', 'pathfinder', 'deadeye'],
-    'scion': ['ascendant'],
-    'shadow': ['saboteur', 'trickster', 'assassin'],
-    'templar': ['hierophant', 'inquisitor', 'guardian'],
-    'witch': ['necromancer', 'occultist', 'elementalist'],
-}
-titles = {
-    'duelist': 'handsome ',
-    'marauder': 'brave ',
-    'ranger': 'ferocious ',
-    'scion': 'bitter ',
-    'shadow': 'tricky ',
-    'templar': 'faithful ',
-    'witch': 'insane '
-}
-skills = {
+def capitalize_all(string):
+    new_string = string.split(' ')
+    for word in new_string:
+        word.capitalize()
+    return ' '.join(map(str, new_string))
 
-}
-spells = {
+bonecos = gems.bonecos
+titles = gems.titles
+ascensoes = gems.ascensoes
+skills = gems.skills
 
-}
-
+# pick char
 bonecos.sort()
 shuffle(bonecos)
 classe = choice(bonecos)
 titulo = titles[classe]
 
+# pick ascension
 ascensoes = ascensoes[classe]
 ascensoes.sort()
 shuffle(ascensoes)
 ascensao = choice(ascensoes)
 
+# pick skill
+skills.sort()
+shuffle(skills)
+main_skill = choice(skills)
+
 # chat part
-print("Exile, my brothers, sisters and I need your help.")
+print("Exile, we need your help.")
 sleep(1)
-print("You, my " + titulo + classe + ", is the one to come in our aid.")
+print("You, my " + titulo.capitalize() + classe.capitalize() + ", is the one to come in our aid.")
 sleep(1)
-print("with your " + ascensao + " skills, you may save Oriath from those who got influenced by the Eldritch curse.")
+print("with your " + ascensao.capitalize() + " skills, you may save Oriath from those who got influenced by the Eldritch curse.")
+sleep(1)
+print("I hope you can save us, sice you're the best with your " + capitalize_all(main_skill) + " ability.")
